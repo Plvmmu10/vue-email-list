ab40e1c1e7f1ea69ed78f3ef4d1619fe1377bfe1
peppeluc22 <gluca0260@gmail.com>
2023-04-11 17:59:50 +0200

    fixed loading config

--- js/script.js
+++ js/script.js
@@ -7,26 +7,35 @@ createApp({
             rndEmail: [],
             rndUser: [],
             numEmail: 0,
-            numUser: 0
+            numUser: 0,
+            emailLoading: false,
+            userLoading: false
         }
     },
     methods: {
         getEmail() {
             this.rndEmail = [];
+            this.emailLoading = true;
 
             for (i = 0; i < this.numEmail; i++) {
                 axios.get(`${this.basePath}random/mail`).then((res) => {
                     this.rndEmail.push(res.data.response);
+                    if (this.rndEmail.length === this.numEmail) {
+                        this.emailLoading = false;
+                    }
                 });
             }
-
         },
         getUsername() {
             this.rndUser = [];
+            this.userLoading = true;
 
             for (i = 0; i < this.numUser; i++) {
                 axios.get(`${this.basePath}random/word`).then((res) => {
                     this.rndUser.push(res.data.response);
+                    if (this.rndUser.length === this.numUser) {
+                        this.userLoading = false;
+                    }
                 });
             }
         }

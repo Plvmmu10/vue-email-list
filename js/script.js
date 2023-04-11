403be2f5462b5764a9bd6d8469c6a134c2e4ed08
peppeluc22 <gluca0260@gmail.com>
2023-04-11 14:18:22 +0200

    added getUsername method

--- js/script.js
+++ js/script.js
@@ -5,19 +5,29 @@ createApp({
         return {
             basePath: 'https://flynn.boolean.careers/exercises/api/',
             rndEmail: [],
+            rndUser: [],
+            numItems: 0
         }
     },
     methods: {
         getEmail() {
             this.rndEmail = [];
 
-            for (i = 0; i < 10; i++) {
+            for (i = 0; i < this.numItems; i++) {
                 axios.get(`${this.basePath}random/mail`).then((res) => {
                     this.rndEmail.push(res.data.response);
                 });
             }
 
         },
+        getUsername() {
+            this.rndUser = [];
 
+            for (i = 0; i < this.numItems; i++) {
+                axios.get(`${this.basePath}random/word`).then((res) => {
+                    this.rndUser.push(res.data.response);
+                });
+            }
+        }
     }
 }).mount('#app');
